8b3b2a288b9f6fe6355de1694f5ad9315b13f554
Surya Gouthu <suryagouthu@suryas-air.lan>
2024-08-20 10:30:13 -0500

    Change in loaderComponent.jsx

--- src/LoaderComponent.jsx
+++ src/LoaderComponent.jsx
@@ -2,8 +2,11 @@ import { Button, Spinner } from "react-bootstrap";
 
 const LoaderComponent = () => {
   return (
-    <div className="d-flex w-100 justify-content-center " style={{}}>
-      <Button variant="primary" disabled>
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "60vh", width: "100%" }}
+    >
+      <Button variant="primary" style={{ width: "60%" }} disabled>
         <Spinner
           as="span"
           animation="grow"
